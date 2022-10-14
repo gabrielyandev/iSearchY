608032a47ee2d1ea25884fa178e3ef410a05ccca
Gabriel Yan <yanjob.dev@gmail.com>
2022-10-14 14:08:23 -0300

    isearchy-o-inicio

---
+++ php/conexao.php
@@ -0,0 +1,11 @@
+<?php
+
+$servidor = "localhost";
+$login = "root";
+$senha = "";
+$nomedobanco = "isearch";
+
+$conexao = new mysqli($servidor, $login, $senha, $nomedobanco)
+// acessa o objeto mysql
+
+?>
